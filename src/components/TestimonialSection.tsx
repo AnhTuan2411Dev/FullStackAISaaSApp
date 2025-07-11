f1a8a476878465fef84208eb6fe454862263a4c7
AnhTuan2411Dev <anhtuan2411.dev@gmail.com>
2025-07-11 18:12:48 +0700

    Refactor TestimonialSection component layout and improve text overflow handling; remove unused React imports from RemoveBackground and WriteArticle pages.

--- src/components/TestimonialSection.tsx
+++ src/components/TestimonialSection.tsx
@@ -10,7 +10,7 @@ const TestimonialSection = () => {
             </div>
             <div className='flex flex-wrap mt-10 justify-center'>
                 {dummyTestimonialData.map((testimonial, index) => (
-                    <div key={index} className='p-8 m-4 max-w-xs rounded-lg bg-[#FDFDFE] shadow-lg border border-gray-100 hover:-translate-y-1 transition duration-300 cursor-pointer'>
+                    <div key={index} className='p-8 m-4 max-w-xs rounded-lg bg-[#FDFDFE] shadow-lg border border-gray-100 hover:-translate-y-1 transition duration-300 cursor-pointer h-75 flex flex-col'>
                         <div className="flex items-center gap-1">
                             {Array(5).fill(0).map((_, index) => (
                                 <img 
@@ -21,7 +21,7 @@ const TestimonialSection = () => {
                                 />
                             ))}
                         </div>
-                        <p className='text-gray-500 text-sm my-5'>"{testimonial.content}"</p>
+                        <p className='text-gray-500 text-sm my-5 flex-1 overflow-hidden'>"{testimonial.content}"</p>
                         <hr className='mb-5 border-gray-300' />
                         <div className='flex items-center gap-4'>
                             <img src={testimonial.image} className='w-12 object-contain rounded-full' alt='' />
