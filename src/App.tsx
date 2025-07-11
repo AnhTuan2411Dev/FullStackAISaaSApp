16a5a8a18e6bfdf8f87f85f05bf637b2621af158
AnhTuan2411Dev <anhtuan2411.dev@gmail.com>
2025-07-11 14:46:06 +0700

    Update project structure and add new features
    - Updated VITE_CLERK_PUBLISHABLE_KEY in .env file.
    - Changed favicon path in index.html and updated the title.
    - Added new dependencies: lucide-react and react-router-dom.
    - Implemented routing in App.tsx with multiple pages including Home, Dashboard, BlogTitles, and more.
    - Created new components for Navbar, Footer, and various pages.
    - Added assets including images and SVGs.
    - Introduced a layout structure with Outlet for nested routing.
    - Added initial styles and fonts in index.css.
    - Created placeholder components for future development.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
 
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+
+// Import all pages
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import BlogTitles from './pages/BlogTitles'
+import WriteActicle from './pages/WriteActicle'
+import Layout from './pages/Layout'
+import RemoveObject from './pages/RemoveObject'
+import GenerateImages from './pages/GenerateImages'
+import RemoveBackground from './pages/RemoveBackground'
+import ReviewResume from './pages/ReviewResume'
+import Community from './pages/Community'
 
 function App() {
-
   return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/ai" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="blog-titles" element={<BlogTitles />} />
+        <Route path="write-article" element={<WriteActicle />} />
+        <Route path="remove-object" element={<RemoveObject />} />
+        <Route path="generate-images" element={<GenerateImages />} />
+        <Route path="remove-background" element={<RemoveBackground />} />
+        <Route path="review-resume" element={<ReviewResume />} />
+        <Route path="community" element={<Community />} />
+      </Route>
+    </Routes>
   )
 }
 
---
+++ src/pages/WriteActicle.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WriteActicle = () => {
+    return (
+        <div>
+            <h1>WriteActicle</h1>
+        </div>
+    )
+}
+
+export default WriteActicle
